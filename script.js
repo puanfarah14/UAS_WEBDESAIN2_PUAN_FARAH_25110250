cd0119dbf93fedd6780f837ae201217d41ab87fa
puanfarah14 <puanfarahnabila352@gmail.com>
2026-07-19 16:44:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("TR Roti Canai");
+
+function showPage(pageId) {
+
+    document.querySelectorAll('.page').forEach(page => {
+        page.classList.remove('active');
+    });
+
+    document.getElementById('page-' + pageId).classList.add('active');
+
+    const carousel = document.getElementById("carouselCanai");
+    const judul = document.getElementById("judulKeunggulan");
+
+    if(pageId === "home"){
+        carousel.style.display = "block";
+        judul.style.display = "block";
+    }else{
+        carousel.style.display = "none";
+        judul.style.display = "none";
+    }
+
+    window.scrollTo(0,0);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    showPage("home");
+});
